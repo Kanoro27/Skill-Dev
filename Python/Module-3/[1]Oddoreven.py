d46c4a07736a228e81d10419983a13f2865420ba
Praveen Kumar S <78217016+Kanoro27@users.noreply.github.com>
2022-08-11 20:06:29 +0530

    Exercise 1

---
+++ Python/Module-3/[1]Oddoreven.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+num = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if num%2==0 :
+    print("This is an even number.")
+else :
+    print("This is an odd number.")
