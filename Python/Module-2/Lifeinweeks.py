2e9349c048d0aa3ea2eab4412fc29da4199755cb
Praveen Kumar S <78217016+Kanoro27@users.noreply.github.com>
2022-08-11 20:05:30 +0530

    Exercise 3

---
+++ Python/Module-2/Lifeinweeks.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+rem=90-int(age)
+z=rem*12
+y=rem*52
+x=rem*365
+print("you have ",x,"days, ",y,"weeks, and ",z,"months left.")
